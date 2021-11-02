be22eab21457b7a2ae7196dca9970a8b3fdd5425
Orlando Urbano Trejo <91504341+OrlandoUrbanoTrejov@users.noreply.github.com>
2021-11-02 11:15:02 -0600

    Add files via upload

---
+++ HolaMundo.java
@@ -0,0 +1,16 @@
+public class HolaMundo{
+	/**
+  	*
+	*Apicacion Java
+	*Title	        :Hola mundo en Java
+	*Description	:Hola mundo en Java desde Vim en Windows
+	*Author		:Orlando Urbano Trejo (Lando)
+	*Date		:05_10_2021
+	*Version	:1
+	*User		:C:\Users\DELL\Documents\Programas_Java
+	*/
+		public static void main (String []args){
+			System.out.println("Hola Mundo En Java Desde Vim En Windows Orlando");
+			//prints Hello World
+		}
+}
